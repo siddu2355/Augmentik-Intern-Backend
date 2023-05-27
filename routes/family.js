8eda38a056e67161e835cd24f662e5d5b4042142
siddu2355 <siddusirasanambeti@gmail.com>
2023-05-27 22:18:30 +0530

    any time last one

--- routes/family.js
+++ routes/family.js
@@ -26,7 +26,7 @@ router.get("/", async (req, res) => {
   res.send(members);
 });
 
-router.post("/", [permission], async (req, res) => {
+router.post("/", async (req, res) => {
   const { error } = validateMember(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
